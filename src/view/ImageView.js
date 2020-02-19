74e2c24a2af82d005f55311b136bb35ddc7c2052
rcoarite <ncoarite@adsib.gob.bo>
2020-02-19 14:14:12 -0400

    Mejroas en ImageView

--- src/view/ImageView.js
+++ src/view/ImageView.js
@@ -3,8 +3,9 @@ class ImageView extends View{
     // scaleType: LayoutInflater.FIT_XY,
     constructor(context){
         super(context);
-        // src: null,
-        // scaleType: LayoutInflater.FIT_XY,
+        this.src = null;
+        this.image = null;
+        this.scaleType = LayoutInflater.FIT_XY;
         this.name = "ImageView";
     }
     // @Override
@@ -16,10 +17,46 @@ class ImageView extends View{
         super.parse(nodeXml);
         this.src = nodeXml.getAttribute(LayoutInflater.ATTR_SRC);
         this.scaleType = nodeXml.getAttribute(LayoutInflater.ATTR_SCALE_TYPE)||LayoutInflater.FIT_XY;
+        this.image = null;
     }
-    setImageFromBase64(txtImageBase64) {
-        this.elemDom.setAttribute(LayoutInflater.ATTR_SRC, 'data:image/png;base64,' + txtImageBase64);
+    // @Override
+    async loadResources() {
+        await super.loadResources();
+        this.image = await Resource.loadImage(this.drawableResource);
+        this.elemIcon.src = image.src;
     }
+    // @Override
+    async onMeasureSync(maxWidth, maxHeight) {
+        await super.onMeasureSync(maxWidth,maxHeight);
+        switch (scaleType){
+            case LayoutInflater.FIT_CENTER: // Escalar la imagen 
+                if(this.elemXml.getAttribute(LayoutInflater.ATTR_LAYOUT_WIDTH)===LayoutInflater.MATCH_PARENT){
+                    this.imgElem.setAttribute("width",maxWidth-this.margin.left-this.margin.right-this.padding.left-this.padding.right);
+                    this.imgElem.setAttribute("height","auto");
+                }
+                else{
+                    this.imgElem.setAttribute("width","auto");
+                    this.imgElem.setAttribute("height","auto"); 
+                }
+                break;
+            case LayoutInflater.FIT_XY:
+                console.log("Ajustando a este tipo");
+                if(this.elemXml.getAttribute(LayoutInflater.ATTR_LAYOUT_WIDTH)===LayoutInflater.MATCH_PARENT){
+                    this.imgElem.setAttribute("width",maxWidth-this.margin.left-this.margin.right-this.padding.left-this.padding.right);
+                    this.imgElem.setAttribute("height","auto");
+                }
+                else
+                {
+                    this.imgElem.setAttribute("width","auto");
+                    this.imgElem.setAttribute("height","auto");
+                }
+                break;
+        }
+    }
+    
+    // setImageFromBase64(txtImageBase64) {
+    //     this.elemDom.setAttribute(LayoutInflater.ATTR_SRC, 'data:image/png;base64,' + txtImageBase64);
+    // }
     async setSyncImageFromURL(urlImage, onLoaded) {
         this.src = urlImage;
         if (this.src !== null) {
